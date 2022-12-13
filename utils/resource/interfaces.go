8a341da4548aefd1fc4d085cd164446468d0228c
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2022-12-13 11:02:33 +0000

    linting

--- utils/resource/interfaces.go
+++ utils/resource/interfaces.go
@@ -2,6 +2,8 @@
  * Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+// Package resource provides helpers related to API resources.
 package resource
 
 // IResource describe an API resource.
