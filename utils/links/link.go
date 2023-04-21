aae6f092c20f591a7c1d62b59b0c44182d274882
acabarbaye <acabarbaye@users.noreply.github.com>
2023-04-21 13:28:22 +0000

    :sparkles: Automatic changes -> 👷 Golang version updates

--- utils/links/link.go
+++ utils/links/link.go
@@ -1,6 +1,14 @@
+/*
+ * Copyright (C) 2020-2023 Arm Limited or its affiliates and Contributors. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Package links provides utilities to deal with links
 package links
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 // SerialiseLink serialises links into JSON strings
 func SerialiseLink(links any) (str string) {
@@ -12,5 +20,8 @@ func SerialiseLink(links any) (str string) {
 		return
 	}
 	str = string(linkStr)
+	if str == "\"\"" {
+		str = ""
+	}
 	return
 }
--- utils/links/link_test.go
+++ utils/links/link_test.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2020-2023 Arm Limited or its affiliates and Contributors. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package links
 
 import (
@@ -10,8 +15,11 @@ import (
 )
 
 func TestSerialiseLink(t *testing.T) {
+	assert.Empty(t, SerialiseLink(nil))
+	assert.Empty(t, SerialiseLink(""))
 	link, err := linkstest.NewFakeLink()
 	require.NoError(t, err)
 	assert.NotEmpty(t, SerialiseLink(link))
+	assert.NotEmpty(t, SerialiseLink(&link))
 
 }
