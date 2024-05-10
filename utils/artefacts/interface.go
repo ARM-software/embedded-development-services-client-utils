9df512eac06f89986c4138a0503bba4ac9fbe3da
Josh <josh.jennings@arm.com>
2024-05-10 10:09:40 +0100

    Create interface.go

---
+++ utils/artefacts/interface.go
@@ -0,0 +1,13 @@
+package artefacts
+
+import (
+	"context"
+
+	"github.com/ARM-software/embedded-development-services-client/client"
+)
+
+//go:generate mockgen -destination=../mocks/mock_$GOPACKAGE.go -package=mocks github.com/ARM-software/embedded-development-services-client-utils/$GOPACKAGE IArtefactManager
+
+type IArtefactManager interface {
+	DownloadJobArtefact(ctx context.Context, jobName string, outputDirectory string, artefactManagerItem client.HalLinkData) (err error)
+}
