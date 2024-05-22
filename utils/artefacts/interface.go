ba97e96862f758301bd016a5838e45b159eb1a23
Josh <josh.jennings@arm.com>
2024-05-22 11:29:36 +0100

    :sparkles: Add reusable manager for downloading artefacts (#64)
    <!--
    Copyright (C) 2020-2022 Arm Limited or its affiliates and Contributors.
    All rights reserved.
    SPDX-License-Identifier: Proprietary
    -->
    ### Description
    
    <!--
    Please add any detail or context that would be useful to a reviewer.
    -->
    
    Add reusable manager for downloading artefacts
    
    ### Test Coverage
    
    <!--
    Please put an `x` in the correct box e.g. `[x]` to indicate the testing
    coverage of this change.
    -->
    
    - [x]  This change is covered by existing or additional automated tests.
    - [ ] Manual testing has been performed (and evidence provided) as
    automated testing was not feasible.
    - [ ] Additional tests are not required for this change (e.g.
    documentation update).
    
    ---------
    
    Co-authored-by: joshjennings98 <joshjennings98@users.noreply.github.com>
    Co-authored-by: Adrien CABARBAYE <adrien.cabarbaye@arm.com>

---
+++ utils/artefacts/interface.go
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020-2024 Arm Limited or its affiliates and Contributors. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package artefacts
+
+import (
+	"context"
+
+	"github.com/ARM-software/embedded-development-services-client/client"
+)
+
+//go:generate mockgen -destination=../mocks/mock_$GOPACKAGE.go -package=mocks github.com/ARM-software/embedded-development-services-client-utils/utils/$GOPACKAGE IArtefactManager
+
+type IArtefactManager interface {
+	DownloadJobArtefact(ctx context.Context, jobName string, outputDirectory string, artefactManagerItem client.HalLinkData) (err error)
+}
