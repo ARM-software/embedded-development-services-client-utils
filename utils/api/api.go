a90b7b53035201326bc9a14dc55370e5e1323eb1
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2025-06-26 07:44:32 +0100

    linting

--- utils/api/api.go
+++ utils/api/api.go
@@ -9,33 +9,34 @@ package api
 import (
 	"context"
 	"fmt"
-	_http "net/http"
+	"net/http"
 	"reflect"
 	"strings"
+
+	"github.com/perimeterx/marshmallow"
 
 	"github.com/ARM-software/embedded-development-services-client-utils/utils/errors"
 	"github.com/ARM-software/golang-utils/utils/commonerrors"
 	"github.com/ARM-software/golang-utils/utils/parallelisation"
 	"github.com/ARM-software/golang-utils/utils/reflection"
 	"github.com/ARM-software/golang-utils/utils/safeio"
-	"github.com/perimeterx/marshmallow"
 )
 
 const requiredFieldError = "no value given for required property"
 
 // IsCallSuccessful determines whether an API response is successful or not
-func IsCallSuccessful(r *_http.Response) bool {
+func IsCallSuccessful(r *http.Response) bool {
 	if r == nil {
 		return false
 	}
-	return r.StatusCode >= _http.StatusOK && r.StatusCode < _http.StatusMultipleChoices
+	return r.StatusCode >= http.StatusOK && r.StatusCode < http.StatusMultipleChoices
 }
 
 // CheckAPICallSuccess verifies whether an API response is successful or not and if not, populates an error with all the information needed.
 // errorContext corresponds to the description of what led to the error if error there is e.g. `Failed adding a user`.
 // resp corresponds to the HTTP response from a certain endpoint. The body of such response is not closed by this function.
 // apiErr corresponds to the error which may be returned by the HTTP client when calling the endpoint.
-func CheckAPICallSuccess(ctx context.Context, errorContext string, resp *_http.Response, apiErr error) (err error) {
+func CheckAPICallSuccess(ctx context.Context, errorContext string, resp *http.Response, apiErr error) (err error) {
 	err = parallelisation.DetermineContextError(ctx)
 	if err != nil {
 		return
@@ -67,7 +68,7 @@ func CheckAPICallSuccess(ctx context.Context, errorContext string, resp *_http.R
 // CallAndCheckSuccess is a wrapper for making an API call and then checking success with `CheckAPICallSuccess`
 // errorContext corresponds to the description of what led to the error if error there is e.g. `Failed adding a user`.
 // apiCallFunc corresponds to a generic function that will be called to make the API call
-func CallAndCheckSuccess[T any](ctx context.Context, errorContext string, apiCallFunc func(ctx context.Context) (*T, *_http.Response, error)) (result *T, err error) {
+func CallAndCheckSuccess[T any](ctx context.Context, errorContext string, apiCallFunc func(ctx context.Context) (*T, *http.Response, error)) (result *T, err error) {
 	if err = parallelisation.DetermineContextError(ctx); err != nil {
 		return
 	}
@@ -99,7 +100,7 @@ func CallAndCheckSuccess[T any](ctx context.Context, errorContext string, apiCal
 	return
 }
 
-func checkResponseMarshalling(ctx context.Context, apiErr error, resp *_http.Response, result any) (err error) {
+func checkResponseMarshalling(ctx context.Context, apiErr error, resp *http.Response, result any) (err error) {
 	if apiErr != nil {
 		err = commonerrors.WrapError(commonerrors.ErrMarshalling, apiErr, "API call was successful but an error occurred during response marshalling")
 		if commonerrors.CorrespondTo(apiErr, requiredFieldError) {
@@ -128,7 +129,7 @@ func checkResponseMarshalling(ctx context.Context, apiErr error, resp *_http.Res
 // T must be an interface.
 // errorContext corresponds to the description of what led to the error if error there is e.g. `Failed adding a user`.
 // apiCallFunc corresponds to a generic function that will be called to make the API call
-func GenericCallAndCheckSuccess[T any](ctx context.Context, errorContext string, apiCallFunc func(ctx context.Context) (T, *_http.Response, error)) (result T, err error) {
+func GenericCallAndCheckSuccess[T any](ctx context.Context, errorContext string, apiCallFunc func(ctx context.Context) (T, *http.Response, error)) (result T, err error) {
 	if err = parallelisation.DetermineContextError(ctx); err != nil {
 		return
 	}
